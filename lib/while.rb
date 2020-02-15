4ab4c7fbdacbcee7bbc1b8b973912f1952d0a7ae
Joseph Choi <jdc07@live.com>
2020-02-15 05:34:57 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,7 @@
 def using_while
   levitation_force = 6
 	while levitation_force < 10
-	  puts "Wingardium Leviosa"
-	  Wingardium Leviosa += 1
-	  e5
+	puts "Wingardium Leviosa"
+	Wingardium Leviosa += 1
 end
 
